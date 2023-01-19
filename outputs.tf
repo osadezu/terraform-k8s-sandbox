7af878bced11b7c59aafd9843ff8804682c76d4b
Oscar Sánchez de Zulueta <osadezu@gmail.com>
2023-01-19 16:13:55 -0500

    Render both policies

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
-output "rendered_policy" {
+output "rendered_cluster_policy" {
   value = data.aws_iam_policy_document.cluster_trust_policy.json
+}
+
+output "rendered_worker_policy" {
+  value = data.aws_iam_policy_document.worker_trust_policy.json
 }
